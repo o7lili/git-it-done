1b3b4ccbf8942f08000c038597090d4a0e29402f
lili <o7lilip@gmail.com>
2022-07-06 19:23:20 -0400

    pushing to pull from laptop hopefully

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,32 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
